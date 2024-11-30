c07bc54be5bd640cd8116941e1389c8000973ee6
t1hub2 <solavolodimir@gmail.com>
2024-11-30 10:20:16 +0100

    Update Code brub.lua

--- Code brub.lua
+++ Code brub.lua
@@ -1,149 +1,35 @@
---[[
-	WARNING: Heads up! This script has not been verified by ScriptBlox. Use at your own risk!
-]]
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "T1 hub / Первый русский скрипт!!", HidePremium = false, IntroText = "T1 hub" , SaveConfig = true, ConfigFolder = "OrionTest"})
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
-OrionLib:MakeNotification({
- Name = "Предупреждение",
- Content = "Все скрипты взяты из Legit mods offical discord! Даня не убивай меня :(",
- Image = "rbxassetid://4483345998",
- Time = 5
-})
-local Tab = Window:MakeTab({
-Name = "Предметы / Скрипты",
-Icon = "rbxassetid://4483345998",
-PremiumOnly = false
-})
+local Window = Rayfield:CreateWindow({
+   Name = "T1 hub / Первый русский скрипт",
+   Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
+   LoadingTitle = "Загрузка",
+   LoadingSubtitle = "T1",
+   Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
 
-Tab:AddButton({
-Name = "Банка мочи",
-Callback = function()
-      print("Executed Star Jug")
- loadstring(game:HttpGet("https://raw.githubusercontent.com/notpoiu/Scripts/refs/heads/main/StarJug.lua"))() 
-  end    
-})
+   DisableRayfieldPrompts = false,
+   DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
 
-Tab:AddButton({
-Name = "Заспавни Пандениона",
-Callback = function()
-      print("Заспавнился")
-loadstring(game:HttpGet("https://raw.githubusercontent.com/DripCapybara/Doors-Mode-Remakes/refs/heads/main/PandemoniumProtected.lua"))() 
-   end    
-})
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = nil, -- Create a custom folder for your hub/game
+      FileName = "T1 Hub"
+   },
 
-Tab:AddButton({
-Name = "Синий портал (не работает на телефонах)",
-Callback = function()
-      print("Executed Star Jug")
- loadstring(game:HttpGet("https://pastebin.com/raw/HT8jgqAY"))() 
-  end    
-})
+   Discord = {
+      Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
+      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
+      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+   },
 
-Tab:AddButton({
-Name = "Золотой шейклайт",
-Callback = function()
-      print("Executed Star Jug")
- loadstring(game:HttpGet(("https://raw.githubusercontent.com/aadyian9000/the-thing/main/GoldenGummyFlashlight.lua"),true))() 
-  end    
-})
-
-Tab:AddButton({
-Name = "Банка воды",
-Callback = function()
-      print("Executed Star Jug")
- loadstring(game:HttpGet('https://gist.githubusercontent.com/IdkMyNameLoll/04d7dd5e02688624b958b8c2604b924c/raw/9e86b34249f44ed2dd433176e67daaf3db30cde8/MoonBottle'))() 
-  end    
-})
-
-Tab:AddButton({
-Name = "Сикибиди пушка",
-Callback = function()
-      print("Executed Star Jug")
-loadstring(game:HttpGet("https://raw.githubusercontent.com/notpoiu/Scripts/main/seekgun.lua"))() 
-  end    
-})
-
-Tab:AddButton({
-Name = "Бомба ракета 2007",
-Callback = function()
-      print("Executed Star Jug")
- loadstring(game:HttpGet("https://raw.githubusercontent.com/notpoiu/Scripts/main/rocketLauncher.lua"))() 
-  end    
-})
-
-Tab:AddButton({
-Name = "Черная дыра",
-Callback = function()
-      print("Executed Star Jug")
- loadstring(game:HttpGet("https://raw.githubusercontent.com/iimateiYT/Scripts/main/Black%20Hole.lua"))() 
-  end    
-})
-
-Tab:AddButton({
-Name = "Сик морф",
-Callback = function()
-      print("Executed Star Jug")
- loadstring(game:HttpGet("https://api.hugebonus.xyz/scripts/TrollFaceSpawner.lua"))() 
-  end    
-})
-
-local Section = Tab:AddSection({
-Name = "Читы"
-})
-
-Tab:AddButton({
-Name = "Мс паинт v3 (не обновляется)",
-Callback = function()
-      print("Executed Star Jug")
- loadstring(game:HttpGet("https://raw.githubusercontent.com/notpoiu/mspaint/main/main.lua"))() 
-  end    
-})
-
-Tab:AddButton({
-Name = "Морфы",
-Callback = function()
-      print("Executed Star Jug")
- loadstring(game:HttpGet("https://raw.githubusercontent.com/ChronoAccelerator/Public-Scripts/main/Morphing/MorphScript.lua"))(); 
-  end    
-})
-
-local Section = Tab:AddSection({
-Name = "Приколы"
-})
-
-Tab:AddButton({
-Name = "ЛЕТИ ПЧЕЛКА!!",
-Callback = function()
-      print("Executed Star Jug")
- loadstring(game:HttpGet("https://raw.githubusercontent.com/T1hub/Reset-script/refs/heads/main/README.md?token=GHSAT0AAAAAAC2GPNNOAXJGZDV5QKEMNIPKZZOYBBA"))() 
-  end    
-})
-
-local Section = Tab:AddSection({
-Name = "Моды"
-})
-
-Tab:AddButton({
-Name = "Жуткий мод",
-Callback = function()
-      print("Executed Star Jug")
- loadstring(game:HttpGet('https://raw.githubusercontent.com/Anzor5458/Mode/refs/heads/main/Creepy%20mode'))() 
-  end    
-})
-
-Tab:AddButton({
-Name = "хаос",
-Callback = function()
-      print("Executed Star Jug")
- loadstring(game:HttpGet("https://raw.githubusercontent.com/t1hub2/Chaos/3adc3d35de10eb716d85cef84b18f29c2a0b3278/README.lua"))() 
-  end    
-})
-
-Tab:AddButton({
-Name = "Хард мод +5 монстров",
-Callback = function()
-      print("Executed Star Jug")
- loadstring(game:HttpGet("https://raw.githubusercontent.com/DripCapybara/Doors-Mode-Remakes/refs/heads/main/HardcoreFixed.lua"))() 
-  end    
+   KeySystem = false, -- Set this to true to use our key system
+   KeySettings = {
+      Title = "Untitled",
+      Subtitle = "Key System",
+      Note = "No method of obtaining the key is provided", -- Use this to tell the user how to get a key
+      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+      Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+   }
 })

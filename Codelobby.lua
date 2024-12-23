da56012b2f8527cde9378c6b3155a3b5c40a043a
t1hub2 <solavolodimir@gmail.com>
2024-12-23 08:55:13 +0100

    Update Codelobby.lua

--- Codelobby.lua
+++ Codelobby.lua
@@ -1,6 +1,6 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 local Window = Rayfield:CreateWindow({
-   Name = "T1 hub / Doors",
+   Name = "T1 hub / Doors [LOBBY]",
    Icon = "codesandbox" , -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
    LoadingTitle = "Загрузка",
    LoadingSubtitle = "T1",

e803ae3eafb64fc66a3ac9510fbca61d8631610a
t1hub2 <solavolodimir@gmail.com>
2024-12-28 17:56:06 +0100

    Update bloxy spawner.lua

--- bloxy spawner.lua
+++ bloxy spawner.lua
@@ -7,7 +7,7 @@ local spawner = loadstring(game:HttpGet("https://raw.githubusercontent.com/Regul
 local entity = spawner.Create({
 	Entity = {
 		Name = "Bloxy",
-		Asset = "https://github.com/t1hub2/Brub/blob/main/bloxy.rbxm",
+		Asset = "https://github.com/t1hub2/Brub/blob/main/bloxyagain.rbxm",
 		HeightOffset = 0
 	},
 	Lights = {

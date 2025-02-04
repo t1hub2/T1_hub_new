ff967f4ccee255a71cf38c48897adf95615cfc71
Mimukauwa <solavolodimir@gmail.com>
2025-02-04 12:06:11 +0100

    Update Brib Tsb.lua

--- Brib Tsb.lua
+++ Brib Tsb.lua
@@ -156,7 +156,7 @@ playAnim2:Play()
    end,
 })
 local Tab = Window:CreateTab("Ульта",4483362458)
-local Label = Tab:CreateLabel("Ульты", 129089434079562, Color3.fromRGB(145, 16, 16), false)
+local Label = Tab:CreateLabel("Ульты", 105053222544471, Color3.fromRGB(0, 0, 0), false)
 local Button = Tab:CreateButton({
    Name = "Сильнейший Герой",
    Callback = function()

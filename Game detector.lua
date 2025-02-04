37ef5f1e9e09f454ab2acc9e44295b29ffde148a
Mimukauwa <solavolodimir@gmail.com>
2025-02-04 13:10:23 +0100

    Update Game detector.lua

--- Game detector.lua
+++ Game detector.lua
@@ -1,6 +1,6 @@
 -- Define the target game IDs and their corresponding URLs
 local gameActions = {
-    [6516141723] = "https://raw.githubusercontent.com/t1hub2/Brub/refs/heads/main/Code%20brub.lua",
+    [6516141723] = "https://raw.githubusercontent.com/t1hub2/T1_hub_new/refs/heads/main/Remake.lua",
     [1234567890] = "https://example.com/script1.lua",
     [9876543210] = "https://example.com/script2.lua"
 }

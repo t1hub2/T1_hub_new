fc9e68190f5c54e4cdf4c456a954810b0f750fce
Mimukauwa <solavolodimir@gmail.com>
2025-02-04 11:55:28 +0100

    Update Brib Tsb.lua

--- Brib Tsb.lua
+++ Brib Tsb.lua
@@ -174,3 +174,20 @@ playAnim2:Play()
 
    end,
 })
+local Button = Tab:CreateButton({
+   Name = "Хиро хантер",
+   Callback = function()
+    local player = game.Players.LocalPlayer
+repeat wait() until player.Character.Humanoid
+local humanoid = player.Character.Humanoid
+local character = player.Character or player.CharacterAdded:Wait()
+local UserInputService = game:GetService("UserInputService")
+
+local anim2 = Instance.new("Animation")
+anim2.AnimationId = "rbxassetid://12342141464"
+
+local playAnim2 = humanoid:LoadAnimation(anim2)
+playAnim2:Play()
+         
+   end,
+})

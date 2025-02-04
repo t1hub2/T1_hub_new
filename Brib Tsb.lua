6d3f45b9f30e4169824edc8490feff32d5649b09
Mimukauwa <solavolodimir@gmail.com>
2025-02-04 11:50:18 +0100

    Update Brib Tsb.lua

--- Brib Tsb.lua
+++ Brib Tsb.lua
@@ -104,3 +104,20 @@ playAnim2:Play()
 
    end,
 })
+local Button = Tab:CreateButton({
+   Name = "Тот чел с битой хз",
+   Callback = function()
+    local player = game.Players.LocalPlayer
+repeat wait() until player.Character.Humanoid
+local humanoid = player.Character.Humanoid
+local character = player.Character or player.CharacterAdded:Wait()
+local UserInputService = game:GetService("UserInputService")
+
+local anim2 = Instance.new("Animation")
+anim2.AnimationId = "rbxassetid://15957371124"
+
+local playAnim2 = humanoid:LoadAnimation(anim2)
+playAnim2:Play()
+
+   end,
+})
